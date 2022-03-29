ea7321140c5b5fb153f5968f061c2f9477f66e11
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2022-03-29 22:45:02 +0300

    Updates

---
+++ script.js
@@ -0,0 +1,11 @@
+const navIds = ['Home', 'Files', 'Recent', 'Photos', 'Bin', 'Shared'];
+const addSelection = (link) => {
+    for (let Id in navIds) {
+        document.getElementById(navIds[Id]).classList.remove("selected");
+    }
+    document.getElementById(link).classList.add('selected');
+}
+
+function headerCloseMobile() {
+    document.getElementById('header').classList.add('closed');
+}
